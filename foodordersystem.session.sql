577f6c704c0633fc3b21c67b79a4a35ec0bdfe47
Tanisha Rajpurohit <tanisharajpurohit18@gmail.com>
2026-04-15 18:07:26 +0530

    final fix

--- foodordersystem.session.sql
+++ foodordersystem.session.sql
@@ -1,4 +1,3 @@
-
 CREATE TABLE users (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(50),
@@ -17,10 +16,8 @@ CREATE TABLE orders (
 INSERT INTO users (name, email, password)
 VALUES ('Tanisha', 'tanisha@gmail.com', '1234');
 
-
 INSERT INTO orders (user_id, food_name, quantity)
 VALUES (1, 'Pizza', 2);
 
 SELECT * FROM users;
 SELECT * FROM orders;
-
